1e073379fc17f9c04a26d62423b83a7cd055bc8e
Sofia <72710072+Strnge-cntry@users.noreply.github.com>
2026-02-11 10:34:03 +0100

    Mock Sensor Klasse (#30)
    Co-authored-by: Sofia Voetmand <sofia.voetmand@alumni.fh-aachen.de>

---
+++ source/PflanzenPi.Sensor/Mocks/MockMoistureSensor.cs
@@ -0,0 +1,17 @@
+﻿namespace PflanzenPi.Sensor.Mocks;
+
+public class MockMoistureSensor : Sensor<Moisture>
+{
+    private readonly Timer _timer;
+
+    public MockMoistureSensor()
+    {
+        _timer = new Timer(SimuliereLesen, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(5));
+    }
+    
+    private void SimuliereLesen(object? _)
+    {
+        var moisture = (float) Random.Shared.NextDouble() * 100f;
+        Publish(new Moisture(moisture));
+    }
+}
