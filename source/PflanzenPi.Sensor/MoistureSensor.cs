dfc8697482791786c46222bb3e86dae6450d24c2
Julian Zell <julian.zell@alumni.fh-aachen.de>
2026-02-17 09:59:53 +0100

    Kalibrierte Prozentwerte für Feuchtigkeit hinzugefügt

--- source/PflanzenPi.Sensor/MoistureSensor.cs
+++ source/PflanzenPi.Sensor/MoistureSensor.cs
@@ -32,9 +32,22 @@ public class MoistureSensor : Sensor<Moisture>
     private void ReadFromPi(Object? _)
     {
         var raw = _adc.ReadRaw();
-        float percentage = raw * (Gain / Int16.MaxValue) * (100.0f / MaximumVoltage);
-        Moisture moisture = new Moisture(percentage);
+        float percentage = raw * (Gain / Int16.MaxValue + 1) * (100.0f / MaximumVoltage); // Lieber Int16.MaxValue + 1 da Int16.MaxValue als 32767 definiert ist aber ADS115 technisch mit 32768 arbeitet.
+        /*
+         * Bei Kalibrierung des Sensors ergab sich:
+         * Völlige Nässe -> 24%
+         * Völlige Trockenheit -> 67%
+         *
+         * Um die Werte benutzbar zu machen, bilde ich sie linear umgedreht auf den Zahlenraum von 0-100 ab.
+         * Die neu kalibrierten Werte sehen wie folgt aus:
+         * Völlige Nässe -> 100%
+         * Völlige Trockenheit -> 0%
+         *
+         * Abbildung: f(x)= -(100/(67-24))*(x-67)
+        */
+        float calibratedPercentage = -(100/43)*(percentage-67);
+        Moisture moisture = new Moisture(calibratedPercentage);
         Publish(moisture);
-        Console.WriteLine($"RAW: {raw} PER: {percentage}%");
+        Console.WriteLine($"RAW: {raw} PER: {calibratedPercentage}%");
     }
 }
