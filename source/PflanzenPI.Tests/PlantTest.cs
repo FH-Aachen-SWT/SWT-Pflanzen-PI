305bafdd9b8fe5a8d00012ee97abc4ecce2ae7e6
lukbes1 <lukbes1@gmail.com>
2026-02-17 13:50:59 +0100

    Fix unit test

--- source/PflanzenPI.Tests/PlantTest.cs
+++ source/PflanzenPI.Tests/PlantTest.cs
@@ -1,7 +1,7 @@
 ﻿using FluentAssertions;
-using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.DataCollection;
 using Moq;
 using PflanzenPi.Plants;
+using PflanzenPi.Plants.Behaviours;
 using PflanzenPi.Sensor;
 
 namespace PflanzenPI.Tests;
@@ -16,14 +16,16 @@ public class PlantTest
         Moisture previous = new Moisture(prev);
         Moisture nexti = new Moisture(next);
         var sensorService = new SensorService();
+        var moistureBehaviourMockFactory = new Mock<IMoistureBehaviourFactory>();
         var moistureBehaviourMock = new Mock<IMoistureBehaviour>();
         var moistureSensorMock = new Mock<ISensor<Moisture>>();
         
         sensorService.Register(moistureSensorMock.Object);
+        moistureBehaviourMockFactory.Setup(factory => factory.Create(It.IsAny<PlantType>())).Returns(moistureBehaviourMock.Object);
         moistureBehaviourMock.Setup(s => s.Interpret(previous)).Returns(MoistureStatus.Dry);
         moistureBehaviourMock.Setup(s => s.Interpret(nexti)).Returns(MoistureStatus.Satisfied);
 
-        var plant = new Plant(sensorService, moistureBehaviourMock.Object);
+        var plant = new Plant(sensorService, moistureBehaviourMockFactory.Object);
         var eventRaised = false;
 
         plant.OnMoistureStatusChanged += (MoistureStatus) =>
